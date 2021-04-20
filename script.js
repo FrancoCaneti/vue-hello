ff3380d8f415b151f94cd06410fd4191dd220a74
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-20 16:50:01 +0200

    image

---
+++ script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        myMessagge:'messaggio',
+        messagge:'"Ragazzi, avete fatto del vostro meglio e avete fallito. La lezione è: non tentare mai." Cit. Homer J Simpson',
+        image:'http://www.fumettologica.it/wp-content/uploads/2014/02/10addio.gif',
+        
+    }
+}); 
